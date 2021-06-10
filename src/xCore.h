4b783a1eea7f08ae18e602d3b929e7a606dee952
huytran99 <tranduchuy1010199@gmail.com>
2021-06-10 16:35:16 +0700

    Update lib pca9685

---
+++ src/xCore.h
@@ -0,0 +1,19 @@
+#ifndef XCORE_H
+#define XCORE_H
+
+class Motors{
+	public:
+		void move(int dir, int speed);
+		void stop();
+};
+
+class Servos{
+	public:
+		Servos();
+		void position(int index, int degrees);
+		void rotate();
+		void release(int index);
+		void spin(int index, int direction, int speed);
+};
+
+#endif
