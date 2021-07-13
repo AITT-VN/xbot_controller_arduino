4535b0da463346c62d842be23267fce80460a99d
Duy Thanh <nguyenduythanh2349@gmail.com>
2021-07-13 14:06:55 +0700

    Update DHT library

--- src/DHT.cpp
+++ src/DHT.cpp
@@ -4,19 +4,78 @@ MIT license
 written by Adafruit Industries
 */
 
+#include <math.h>
 #include "DHT.h"
-
-DHT::DHT(uint8_t pin, uint8_t type) {
+//#define NAN 0
+#ifdef DEBUG
+#define DEBUG_PRINT(...)  Serial.println(__VA_ARGS__)
+#else
+#define DEBUG_PRINT(...)
+#endif
+DHT::DHT(uint8_t pin, uint8_t type, uint8_t count) {
   _pin = pin;
   _type = type;
+  _count = count;
   firstreading = true;
 }
 
+
+
 void DHT::begin(void) {
-  // set up the pins!
-  pinMode(_pin, INPUT);
-  digitalWrite(_pin, HIGH);
-  _lastreadtime = 0;
+
+  if(_type == DHT10){
+    if(DHT10Init()){
+        SERIALPRINT.println("Error : Failed to init DHT 11\n");
+		    while(1);
+    }
+  }
+  else{
+    // set up the pins!
+    pinMode(_pin, INPUT);
+    digitalWrite(_pin, HIGH);
+    _lastreadtime = 0;
+  }
+
+}
+
+/**Common  interface to get temp&humi value.support all DHT device.
+ * 
+ * @return 0 for calibrated failed,1 for succeed.
+ **/
+int DHT::readTempAndHumidity(float *data)
+{
+    uint32_t target_val[2] = {0};
+    uint32_t cnt;
+    if(_type == DHT10){
+      while(DHT10ReadStatus()==0)
+      {
+        DHT10Init();
+        delay(30);
+        cnt++;
+        if(cnt > 3)
+          return -1;
+      }
+        //wait for data ready。
+        while(readTargetData(target_val)){
+          cnt++;
+          delay(50);
+          if(cnt > 5){
+            return -1;
+          }
+
+        }
+        data[0] = target_val[0] *100.0/1024/1024;
+        data[1] = target_val[1] *200.0/1024/1024-50;
+
+    }
+    else{
+        data[0] = readHumidity();
+        data[1] = readTemperature();
+        if (isnan(data[0]) || isnan(data[1])){
+            return -1;
+        }
+    }
+    return 0;
 }
 
 //boolean S == Scale.  True == Farenheit; False == Celcius
@@ -45,7 +104,7 @@ float DHT::readTemperature(bool S) {
       return f;
     }
   }
-  Serial.print("Read fail");
+  DEBUG_PRINT("Read fail");
   return NAN;
 }
 
@@ -69,7 +128,7 @@ float DHT::readHumidity(void) {
       return f;
     }
   }
-  Serial.print("Read fail");
+  DEBUG_PRINT("Read fail");
   return NAN;
 }
 
@@ -95,8 +154,8 @@ boolean DHT::read(void) {
   }
   firstreading = false;
   /*
-    Serial.print("Currtime: "); Serial.print(currenttime);
-    Serial.print(" Lasttime: "); Serial.print(_lastreadtime);
+    DEBUG_PRINT("Currtime: "); DEBUG_PRINT(currenttime);
+    DEBUG_PRINT(" Lasttime: "); DEBUG_PRINT(_lastreadtime);
   */
   _lastreadtime = millis();
 
@@ -106,7 +165,7 @@ boolean DHT::read(void) {
   pinMode(_pin, OUTPUT);
   digitalWrite(_pin, LOW);
   delay(20);
-  cli();
+  //cli();
   digitalWrite(_pin, HIGH);
   delayMicroseconds(40);
   pinMode(_pin, INPUT);
@@ -129,23 +188,23 @@ boolean DHT::read(void) {
     if ((i >= 4) && (i%2 == 0)) {
       // shove each bit into the storage bytes
       data[j/8] <<= 1;
-      if (counter > 6)
+      if (counter > _count)
         data[j/8] |= 1;
       j++;
     }
 
   }
 
-  sei();
+  //sei();
   
   /*
-  Serial.println(j, DEC);
-  Serial.print(data[0], HEX); Serial.print(", ");
-  Serial.print(data[1], HEX); Serial.print(", ");
-  Serial.print(data[2], HEX); Serial.print(", ");
-  Serial.print(data[3], HEX); Serial.print(", ");
-  Serial.print(data[4], HEX); Serial.print(" =? ");
-  Serial.println(data[0] + data[1] + data[2] + data[3], HEX);
+  DEBUG_PRINTln(j, DEC);
+  DEBUG_PRINT(data[0], HEX); DEBUG_PRINT(", ");
+  DEBUG_PRINT(data[1], HEX); DEBUG_PRINT(", ");
+  DEBUG_PRINT(data[2], HEX); DEBUG_PRINT(", ");
+  DEBUG_PRINT(data[3], HEX); DEBUG_PRINT(", ");
+  DEBUG_PRINT(data[4], HEX); DEBUG_PRINT(" =? ");
+  DEBUG_PRINTln(data[0] + data[1] + data[2] + data[3], HEX);
   */
 
   // check we read 40 bits and that the checksum matches
@@ -158,3 +217,214 @@ boolean DHT::read(void) {
   return false;
 
 }
+
+/*****************************************************************************/
+/*****************************************************************************/
+
+/**Reset sensor.
+ * @return 0 for calibrated failed,1 for succeed.
+ **/
+int DHT::DHT10Reset(void)
+{
+  if(_type == DHT10)
+	    return i2cWriteByte(RESET_REG_ADDR);
+  else{
+      return 0;
+      SERIALPRINT.println("This function only support for DHT10");
+  } 
+
+}
+
+/** Read status register.check the calibration flag - bit[3]: 1- calibrated ok ,0 - Not calibrated.
+ * 
+ * @return 0 for calibrated failed,1 for succeed. 
+ * 
+ **/
+int DHT::DHT10ReadStatus(void)
+{
+
+	int ret = 0;
+	uint8_t statu = 0;
+  if(_type == DHT10){
+        ret = i2cReadByte(statu);
+	    if(ret)
+		    SERIALPRINT.println("Failed to read byte\n");
+	    if((statu & 0x8)==0x8)  
+  		  return 1;
+  	  else  
+	  	  return 0;
+  }
+  else{
+        SERIALPRINT.println("This function only support for DHT10");
+        return 0;
+  }
+    
+}
+
+/** Init sensor,send 0x08,0x00 to register 0xe1.
+ *  @ return : 0 if success, non-zero if failed.
+ **/
+int DHT::setSystemCfg(void)
+{
+	uint8_t cfg_param[] = {0xe1,0x08,0x00};
+  if(_type == DHT10)
+	    return i2cWriteBytes(cfg_param,sizeof(cfg_param));
+  else {
+    SERIALPRINT.println("This function only support for DHT10");
+    return 0;
+  }
+}
+
+
+/** Read temp & humi result buf from sensor.
+ *  total 6 bytes,the first byte for status register,other 5 bytes for temp & humidity data.
+ *  @ return : 0 if success, non-zero if failed.
+ **/
+int DHT::readTargetData(uint32_t *data)
+{
+	uint8_t statu = 0;
+	uint8_t bytes[6] = {0};
+	uint8_t cfg_params[] ={0xac,0x33,0x00};
+	int ret = 0;
+
+  if(_type == DHT10){
+
+    if( i2cWriteBytes(cfg_params,sizeof(cfg_params)) ){
+      return -1;
+    }
+
+    delay(75);
+    // check device busy flag， bit[7]:1 for busy, 0 for idle.
+    while(statu & 0x80 == 0x80){
+      SERIALPRINT.println("Device busy!");
+      delay(200);
+      if(i2cReadByte(statu)){
+        return -1;
+      }	
+    }
+
+    if(i2cReadBytes(bytes,sizeof(bytes))){
+      return -1;
+    }
+    
+
+    data[HUMIDITY_INDEX] = (data[HUMIDITY_INDEX] | bytes[1]) << 8;
+    data[HUMIDITY_INDEX] = (data[HUMIDITY_INDEX] | bytes[2]) << 8;
+    data[HUMIDITY_INDEX] = (data[HUMIDITY_INDEX] | bytes[3]);
+    data[HUMIDITY_INDEX] = data[HUMIDITY_INDEX] >>4;
+
+    data[TEMPRATURE_INDEX] = (data[TEMPRATURE_INDEX] | bytes[3]) << 8;
+    data[TEMPRATURE_INDEX] = (data[TEMPRATURE_INDEX] | bytes[4]) << 8;
+    data[TEMPRATURE_INDEX] = (data[TEMPRATURE_INDEX] | bytes[5]);
+    data[TEMPRATURE_INDEX] &= 0xfffff;
+
+    return 0;
+  }
+  else {
+    SERIALPRINT.println("This function only support for DHT10");
+    return 0;
+  }
+}
+
+/**DHT10 Init function.
+ * Reset sensor and wait for calibration complete.
+ * @ return : 0 if success, non-zero if failed.
+ **/ 
+int DHT::DHT10Init(void)
+{
+	int ret = 0;
+	int cnt = 0;
+
+  if(_type == DHT10){
+
+      delay(500);
+      DHT10Reset();
+      delay(300);
+      
+      ret = setSystemCfg();
+      if(ret){
+        SERIALPRINT.println("Failed to set system conf reg \n");
+      }
+      //SERIALPRINT.println("Set system cfg OK!");
+
+      delay(500);
+      
+      while(DHT10ReadStatus()==0)
+      {
+        SERIALPRINT.println("get status error!");
+        DHT10Reset();
+        delay(500);
+        if(setSystemCfg()){
+          SERIALPRINT.println("Failed to set system conf reg \n");
+        }
+        delay(500);
+        cnt++;
+        if(cnt>5)
+          return -1;
+      }
+      return 0;
+  }
+  else{
+      SERIALPRINT.println("This function only support for DHT10");
+      return 0;
+  }
+  
+}
+
+
+
+/*****************************************************************************/
+/*****************************************************************************/
+
+int DHT::i2cReadByte(uint8_t& byte)
+{
+	int cnt = 0;
+	Wire.requestFrom(DEFAULT_IIC_ADDR,1);
+	while(1 != Wire.available()){
+		cnt++;
+		if(cnt >= 10 )
+			return -1;
+		delay(1);
+	}
+
+	byte = Wire.read();
+	return 0;
+}
+
+int DHT::i2cReadBytes(uint8_t *bytes,uint32_t len)
+{
+	int cnt = 0;
+	Wire.requestFrom(DEFAULT_IIC_ADDR,len);
+	while(len != Wire.available()){
+		cnt++;
+		if(cnt >= 10 )
+			return -1;
+		delay(1);
+	}
+	for(int i=0;i<len;i++){
+		bytes[i] = Wire.read();
+	}
+	return 0;
+}
+
+
+int DHT::i2cWriteBytes(uint8_t *bytes,uint32_t len)
+{
+	Wire.beginTransmission(DEFAULT_IIC_ADDR);
+	for(int i=0;i< len;i++)
+	{
+		Wire.write(bytes[i]);
+	}
+	return Wire.endTransmission();
+}
+
+int DHT::i2cWriteByte(uint8_t byte)
+{
+	Wire.beginTransmission(DEFAULT_IIC_ADDR);
+	Wire.write(byte);
+	return Wire.endTransmission();
+}
+
+
+
+
