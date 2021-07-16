d937865b209079c726590a6f4cfb9bf6ee8b12a4
Duy Thanh <nguyenduythanh2349@gmail.com>
2021-07-16 18:40:02 +0700

    Update Ultrasonic

--- src/Ultrasonic.cpp
+++ src/Ultrasonic.cpp
@@ -9,8 +9,7 @@
 #include "Arduino.h"
 #include "Ultrasonic.h"
 
-Ultrasonic::Ultrasonic(
-        int triggerPin, int echoPin) {
+Ultrasonic::Ultrasonic(int triggerPin, int echoPin) {
     this->triggerPin = triggerPin;
     this->echoPin = echoPin;
     pinMode(triggerPin, OUTPUT);
@@ -18,11 +17,6 @@ Ultrasonic::Ultrasonic(
 }
 
 double Ultrasonic::measureDistanceCm() {
-    //Using the approximate formula 19.307°C results in roughly 343m/s which is the commonly used value for air.
-    return measureDistanceCm(19.307);
-}
-
-double Ultrasonic::measureDistanceCm(float temperature) {
     // Make sure that trigger pin is LOW.
     digitalWrite(triggerPin, LOW);
     delayMicroseconds(2);
@@ -33,11 +27,31 @@ double Ultrasonic::measureDistanceCm(float temperature) {
     // Measure the length of echo signal, which is equal to the time needed for sound to go there and back.
     unsigned long durationMicroSec = pulseIn(echoPin, HIGH);
 
-    double speedOfSoundInCmPerMs = 0.03313 + 0.0000606 * temperature; // Cair ≈ (331.3 + 0.606 ⋅ ϑ) m/s
+    double speedOfSoundInCmPerMs = 0.03313; // Cair ≈ (331.3 + 0.606 ⋅ ϑ) m/s
     double distanceCm = durationMicroSec / 2.0 * speedOfSoundInCmPerMs;
     if (distanceCm == 0 || distanceCm > 400) {
         return -1.0 ;
     } else {
         return distanceCm;
+    }
+}
+
+double Ultrasonic::measureDistancemm() {
+    // Make sure that trigger pin is LOW.
+    digitalWrite(triggerPin, LOW);
+    delayMicroseconds(2);
+    // Hold trigger for 10 microseconds, which is signal for sensor to measure distance.
+    digitalWrite(triggerPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(triggerPin, LOW);
+    // Measure the length of echo signal, which is equal to the time needed for sound to go there and back.
+    unsigned long durationMicroSec = pulseIn(echoPin, HIGH);
+
+    double speedOfSoundInCmPerMs = 0.03313; // Cair ≈ (331.3 + 0.606 ⋅ ϑ) m/s
+    double distancemm = durationMicroSec *10 / 2.0 * speedOfSoundInCmPerMs;
+    if (distancemm == 0 || distancemm > 4000) {
+        return -1.0 ;
+    } else {
+        return distancemm;
     }
 }
--- src/Ultrasonic.h
+++ src/Ultrasonic.h
@@ -3,36 +3,22 @@
   Created by Martin Sosic, June 11, 2016.
 */
 
-#ifndef Ultrasonic_H
-#define Ultrasonic_H
+#ifndef ULTRASONIC_H
+#define ULTRASONIC_H
 
 #include "Arduino.h"
 
-class Ultrasonic 
-{
+class Ultrasonic{
  public:
     /**
      * @param triggerPin  Digital pin that is used for controlling sensor (output).
      * @param echoPin  Digital pin that is used to get information from sensor (input).
      */
     Ultrasonic(int triggerPin, int echoPin);
-
-    /**
-     * Measures distance by sending ultrasonic waves and measuring time it takes them
-     * to return.
-     * @returns Distance in centimeters, or negative value if distance is greater than 400cm.
-     */
     double measureDistanceCm();
-
-    /**
-     * Measures distance by sending ultrasonic waves and measuring time it takes them
-     * to return.
-     * @param temperature  Temperature in degrees celsius
-     * @returns Distance in centimeters, or negative value if distance is greater than 400cm.
-     */
-    double measureDistanceCm(float temperature);
+    double measureDistanceMm();
  private:
     int triggerPin, echoPin;
 };
 
-#endif // Ultrasonic_H
+#endif // ULTRASONIC_H
